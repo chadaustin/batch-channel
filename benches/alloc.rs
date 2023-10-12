6ddf91e95081059316128794851de7b0ae54c89b
Chad Austin <chad@chadaustin.me>
2023-10-11 23:53:20 -0700

    migrate from tiny-bench to divan

--- benches/alloc.rs
+++ benches/alloc.rs
@@ -1,8 +1,13 @@
-use tiny_bench::bench;
-use tiny_bench::black_box;
+#[divan::bench]
+fn alloc_unbounded() -> (batch_channel::Sender<u8>, batch_channel::Receiver<u8>) {
+    batch_channel::unbounded()
+}
+
+#[divan::bench]
+fn alloc_dealloc_unbounded() {
+    let _ = batch_channel::unbounded::<u8>();
+}
 
 fn main() {
-    bench(|| {
-        _ = black_box(batch_channel::unbounded::<u8>());
-    });
+    divan::main()
 }
