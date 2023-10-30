c4879a4d085f5b4da1014079aebd1d6919e32bf0
Chad Austin <chad@chadaustin.me>
2023-10-29 20:42:08 -0700

    add benchmarks for cloning senders and receivers

--- benches/alloc.rs
+++ benches/alloc.rs
@@ -1,3 +1,5 @@
+use divan::Bencher;
+
 #[divan::bench]
 fn alloc_unbounded() -> (batch_channel::Sender<u8>, batch_channel::Receiver<u8>) {
     batch_channel::unbounded()
@@ -6,6 +8,22 @@ fn alloc_unbounded() -> (batch_channel::Sender<u8>, batch_channel::Receiver<u8>)
 #[divan::bench]
 fn alloc_dealloc_unbounded() {
     let _ = batch_channel::unbounded::<u8>();
+}
+
+#[divan::bench]
+fn clone_rx(bencher: Bencher) {
+    let (_tx, mut rx) = batch_channel::unbounded::<u8>();
+    bencher.bench_local(|| {
+        rx = rx.clone();
+    });
+}
+
+#[divan::bench]
+fn clone_tx(bencher: Bencher) {
+    let (mut tx, _rx) = batch_channel::unbounded::<u8>();
+    bencher.bench_local(|| {
+        tx = tx.clone();
+    });
 }
 
 fn main() {
