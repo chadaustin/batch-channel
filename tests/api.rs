53a46c5058c71634c4ddac7c85714b17c31956e9
Chad Austin <chad@chadaustin.me>
2024-03-03 13:19:40 -0800

    introduce a helper that counts instances

--- tests/api.rs
+++ tests/api.rs
@@ -1,3 +1,5 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+
 #[test]
 fn sync_to_async_and_back() {
     let (tx, rx) = batch_channel::bounded::<()>(1);
@@ -9,4 +11,37 @@ fn sync_to_async_and_back() {
     let rx = rx.into_async();
 
     _ = (tx, rx);
+}
+
+static COUNT: AtomicUsize = AtomicUsize::new(0);
+
+struct Counter {
+    inc: usize,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        let inc = 1;
+        COUNT.fetch_add(inc, Ordering::AcqRel);
+        Counter { inc }
+    }
+}
+
+impl Drop for Counter {
+    fn drop(&mut self) {
+        COUNT.fetch_sub(self.inc, Ordering::AcqRel);
+    }
+}
+
+#[test]
+fn counter_instances_count() {
+    assert_eq!(0, COUNT.load(Ordering::Acquire));
+    let c1 = Counter::new();
+    assert_eq!(1, COUNT.load(Ordering::Acquire));
+    let c2 = Counter::new();
+    assert_eq!(2, COUNT.load(Ordering::Acquire));
+    drop(c1);
+    assert_eq!(1, COUNT.load(Ordering::Acquire));
+    drop(c2);
+    assert_eq!(0, COUNT.load(Ordering::Acquire));
 }
